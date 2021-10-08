f9bfaf3c30ec3779a4bd9abd069ea4c08f5a1813
habib <habibdev96@gmail.com>
2021-10-07 18:06:07 -0600

    initial workflow setup

---
+++ src/abstracts/Globals.js
@@ -0,0 +1,90 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Globals = createGlobalStyle`
+  :root {
+    /* colors */
+    --white: hsl(0, 0%, 100%);
+    --darkCyan: hsl(179, 81%, 29%);
+    --darkGrayBlue: hsl(215, 19%, 25%);
+    --paleOrange: hsl(25, 94%, 86%);
+    --lightCream: hsl(147, 60%, 97%);
+    --gray: hsl(215, 5%, 54%); 
+    
+    /* radius */
+    --mainRadius: 5rem;
+
+    /* grid gap */
+    --gap: 5rem; 
+      
+    /* shadows */
+    --mainShadow: 0.1rem 0.5rem 0.5rem rgba(0, 0, 0, 0.2);
+
+    /* transitions */
+    --mainTransition: all 0.3s ease-in-out;
+
+    /* font sizes */
+    --xxl: 4.5rem;
+    --xl: 4rem;
+    --lg: 3.5rem;
+    --md: 3rem;
+    --sm: 2.5rem;
+    --xs: 2rem;
+    --xxs: 1.6rem;
+  }
+
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    overflow-x: hidden;
+    background-color: var(--lightCream);
+  }
+
+  img {
+    width: 100%;
+    object-fit: cover;
+    pointer-events: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  button {
+    font-family: inherit;
+    cursor: pointer;
+  }
+
+  input {
+    font-family: inherit;
+  }
+
+  @media (prefers-reduced-motion: reduce) {
+    *,
+    *::before,
+    *::after {
+      animation-delay: -1ms !important;
+      animation-duration: 1ms !important;
+      animation-iteration-count: 1 !important;
+      background-attachment: initial !important;
+      scroll-behavior: auto !important;
+      transition-duration: 0s !important;
+      transition-delay: 0s !important;
+    }
+  }
+`;
+
+export default Globals;
---
+++ src/abstracts/Mixins.js
@@ -0,0 +1,92 @@
+import { css } from 'styled-components';
+
+const flexCenter = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: var(--gap);
+`;
+
+const flexAlign = css`
+  display: flex;
+  align-items: center;
+  gap: var(--gap);
+`;
+
+const flexBetween = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: var(--gap);
+`;
+
+const twoCol = css`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  align-items: center;
+  gap: var(--gap);
+`;
+
+const threeCol = css`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  align-items: center;
+  gap: var(--gap);
+`;
+
+const headingStyles = css`
+  font-family: 'Fraunces', serif;
+  font-weight: 900;
+  letter-spacing: 0.1rem;
+  line-height: 1.2;
+`;
+
+const textStyles = css`
+  font-family: 'Barlow', sans-serif;
+  font-weight: 400;
+  letter-spacing: 0.1rem;
+  line-height: 2.2;
+`;
+
+const sectionSpacingSm = css`
+  padding: 5rem 2rem;
+`;
+
+const sectionSpacingMd = css`
+  padding: 10rem 2rem;
+`;
+
+const sectionSpacingLg = css`
+  padding: 20rem 2rem;
+`;
+
+const maxWidthSm = css`
+  max-width: 1000px;
+  margin: 0 auto;
+`;
+
+const maxWidthMd = css`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const maxWidthLg = css`
+  max-width: 1400px;
+  margin: 0 auto;
+`;
+
+export {
+  flexCenter,
+  flexAlign,
+  flexBetween,
+  twoCol,
+  threeCol,
+  headingStyles,
+  textStyles,
+  sectionSpacingSm,
+  sectionSpacingMd,
+  sectionSpacingLg,
+  maxWidthSm,
+  maxWidthMd,
+  maxWidthLg,
+};
